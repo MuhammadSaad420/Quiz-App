7da8c2234ee3c55d3c13eacf0e74a29a269bb625
MuhammadSaad420 <saad.oraxtech@gmail.com>
2023-04-10 13:57:01 +0500

    Adding the button functionality to the question activity

--- app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionActivity.kt
@@ -1,14 +1,24 @@
 package com.example.quizapp
 
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import java.lang.reflect.Type
 
-class QuizQuestionActivity : AppCompatActivity() {
+class QuizQuestionActivity : AppCompatActivity() , View.OnClickListener{
+    private var mQuestionList: ArrayList<Question>? = null;
+    private var mCurrentQuestion: Int = 1;
+    private  var mSelectedOptionsPosition: Int = 0;
+
 
     var tvQuestion: TextView? = null;
     var ivFlag: ImageView? = null;
@@ -33,23 +43,85 @@ class QuizQuestionActivity : AppCompatActivity() {
         tvOption3 = findViewById(R.id.tv_option3);
         tvOption4 = findViewById(R.id.tv_option4);
         btnSubmit = findViewById(R.id.btn_submit);
+        tvOption1?.setOnClickListener(this)
+        tvOption2?.setOnClickListener(this)
+        tvOption3?.setOnClickListener(this)
+        tvOption4?.setOnClickListener(this)
 
-        var questionsList = Constants.getQuestions();
-        var currentQuestion = 1;
-        val question: Question = questionsList[currentQuestion - 1];
-        tvQuestion?.text  = question.questions;
+        setQuestion()
+        setDefaultOption()
+        Log.i("saad",Constants.getQuestions()[1].toString());
+    }
+
+    private fun setDefaultOption() {
+
+        var options = ArrayList<TextView>()
+        tvOption1?.let {
+            options.add(it);
+        }
+        tvOption2?.let {
+            options.add(it);
+        }
+        tvOption3?.let {
+            options.add(it)
+        }
+        tvOption4?.let {
+            options.add(it)
+        }
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this,R.drawable.default_option_bg)
+        }
+    }
+
+    private fun setQuestion() {
+        mQuestionList = Constants.getQuestions();
+        val question: Question = mQuestionList!![mCurrentQuestion - 1];
+        tvQuestion?.text = question.questions;
         ivFlag?.setImageResource(question.image);
-        progressBar?.progress = currentQuestion;
-        tvRemain?.text = "$currentQuestion/${questionsList.size}"
+        progressBar?.progress = mCurrentQuestion;
+        tvRemain?.text = "$mCurrentQuestion/${mQuestionList!!.size}"
         tvOption1?.text = question.questionOne
         tvOption2?.text = question.questionTwo
         tvOption3?.text = question.questionThree
         tvOption4?.text = question.questionFour
+    }
 
+    override fun onClick(v: View?) {
+        when(v?.id) {
+           R.id.tv_option1 -> {
+               tvOption1?.let {
+                   setSelectedOption(it, 1)
+               }
+           }
+            R.id.tv_option2 -> {
+                tvOption2?.let {
+                    setSelectedOption(it, 2)
+                }
+            }
+            R.id.tv_option3 -> {
+                tvOption3?.let {
+                    setSelectedOption(it, 3)
+                }
+            }
+            R.id.tv_option4 -> {
+                tvOption4?.let {
+                    setSelectedOption(it, 4)
+                }
 
+            }
+            R.id.btn_submit -> {
 
+            }
+        }
+    }
 
-
-        Log.i("saad",Constants.getQuestions()[1].toString());
+    private fun setSelectedOption(view: TextView, optionIndex: Int) {
+        setDefaultOption();
+        mSelectedOptionsPosition = optionIndex;
+        view.setTypeface(view.typeface, Typeface.BOLD);
+        view.background = ContextCompat.getDrawable(this,R.drawable.selected_option_bg)
     }
 }
